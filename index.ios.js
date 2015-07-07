eec0d37b3b012bdcf6de6374c874ffb781a21bde
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-06 23:08:08 -0700

    multiple movies loading now

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,9 @@
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -8,34 +14,91 @@ var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
+  fetchData,
 } = React;
 
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+
 
 var awesomeproject = React.createClass({
+
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+
   render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-      </View>
-    );
-  }
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+  );
+
+
+    //  var movie = this.state.movies[0];
+    //  return this.renderMovie(movie);
+   },
+
+   renderLoadingView: function() {
+     return (
+       <View style={styles.container}>
+         <Text>
+           Loading movies...
+         </Text>
+       </View>
+     );
+   },
+
+   renderMovie: function(movie) {
+     return (
+       <View style={styles.container}>
+         <Image
+           source={{uri: movie.posters.thumbnail}}
+           style={styles.thumbnail}
+         />
+         <View style={styles.rightContainer}>
+           <Text style={styles.title}>{movie.title}</Text>
+           <Text style={styles.year}>{movie.year}</Text>
+         </View>
+       </View>
+     );
+   },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -54,6 +117,24 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },  
+
+
 });
 
 AppRegistry.registerComponent('awesomeproject', () => awesomeproject);
